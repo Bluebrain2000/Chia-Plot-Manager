b59bf9d1c7a58990029c57dc1e8122ed8768e1c8
Richard <info@viso.at>
2021-05-20 17:00:51 +0200

    Update plotman.conf.pl

--- plotman.conf.pl
+++ plotman.conf.pl
@@ -8,6 +8,7 @@ $drive_final="F:\\Chia Staging";	# -d option for chia.exe
 $sleeptime=30;		# sleep time in seconds between updates
 $startNewPlot=1;	# if plotmanager should start new plots (normally=1, for debug purpose =0 to prevent the plotmanager to start new plots)
 $check_last=6;		# number of last ... logfiles to parse for calculations
+$age_dead=10;     # hours after a plot is considered dead
 $send_data_to_server=0;	# submit status data to server?
 $server_url="https://example.com/chiaplotlog.pl";
 $stagedrive="F";
